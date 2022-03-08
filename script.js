d10efd28dbb1c7555f638c923834293b2db25e43
jraharris89 <jraharris89@gmail.com>
2022-03-07 16:35:19 -0800

    Phase 4

--- script.js
+++ script.js
@@ -84,3 +84,33 @@ Array.from(aboutMeTextContent).forEach((char) => {
   });
 });
 //END OF About me text
+
+//Projects
+const projects = document.querySelectorAll(".project a");
+projects.forEach((project) => {
+  project.addEventListener("mouseenter", () => {
+    project.firstElementChild.style.top = `-${
+      project.firstElementChild.offsetHeight - project.offsetHeight + 20
+    }px`;
+  });
+  project.addEventListener("mouseleave", () => {
+    project.firstElementChild.style.top = "2rem";
+  });
+});
+//End of Projects
+// Section 4
+document.querySelectorAll(".service-btn").forEach((service) => {
+  service.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const serviceText = service.nextElementSibling;
+    serviceText.classList.toggle("change");
+
+    const rightPosition = serviceText.classList.contains("change")
+      ? `calc(100% - ${getComputedStyle(service.firstElementChild).width})`
+      : 0;
+
+    service.firstElementChild.style.right = rightPosition;
+  });
+});
+// End of Section 4
